fe14a676fd123daf7d7ba9c204116d97d5294e8b
Dra <bakugo.13@gmail.com>
2022-03-23 17:07:32 +0100

    Delete old files

--- src/main/kotlin/map/tiles/Stairs.kt
+++
@@ -1,6 +0,0 @@
-package map.tiles
-
-class Stairs: Tile(null) {
-    override val noEntityDisplay: String
-        get() = "\uD83E\uDE9C"
-}
--- src/main/kotlin/util/graph/architest/corridorgen/CorridorGenerationNode.kt
+++
@@ -1,7 +0,0 @@
-package util.graph.architest.corridorgen
-
-import util.graph.architest.SearchNode
-
-class CorridorGenerationNode(a:Boolean = false): SearchNode(a) {
-
-}
